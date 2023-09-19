c4814f31643810aa5384ad529d598e7bba65d276
Kaplc <755536175@qq.com>
2023-09-19 17:17:58 +0800

    补充提示面板脚本

---
+++ Assets/Script/BeginScene/UI/TipsPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipsPanel : BasePanel
+{
+    public Button btnSure;
+    public Text txTips;
+    
+    protected override void Init()
+    {
+        btnSure.onClick.AddListener(() =>
+        {
+            UIManager.Instance.Hide<TipsPanel>(true, null);
+        });
+    }
+
+    public void UpdateTips(string info)
+    {
+        txTips.text = info;
+    }
+}
