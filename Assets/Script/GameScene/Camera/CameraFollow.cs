d2436ba9055f8f4400d0072a3961a120e9b23737
Kaplc <755536175@qq.com>
2023-09-21 21:06:36 +0800

    完成摄像机跟随

---
+++ Assets/Script/GameScene/Camera/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 offSet;
+    private Vector3 cameraPos;
+    public float lookAtY;
+    public Vector3 lookAtPos;
+
+    public float followSpeed;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // 先向后偏移
+        cameraPos = playerTransform.position + playerTransform.forward * offSet.z;
+        cameraPos += playerTransform.up * offSet.y;
+        cameraPos += playerTransform.right * offSet.x;
+        // 跟随
+        transform.position = Vector3.Lerp(transform.position, cameraPos, Time.deltaTime * followSpeed);
+
+        // 看向
+        lookAtPos = playerTransform.position + Vector3.up * lookAtY;
+        transform.rotation = Quaternion.LookRotation(lookAtPos - transform.position);
+    }
+}
