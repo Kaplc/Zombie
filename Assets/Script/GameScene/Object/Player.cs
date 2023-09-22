50601736bded294641b7d06306477d3910840046
Kaplc <755536175@qq.com>
2023-09-22 09:30:05 +0800

    完成角色移动

--- Assets/Script/GameScene/Object/Player.cs
+++ Assets/Script/GameScene/Object/Player.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Animator animator;
+    public float animaSpeed;
+    
+    private float xDir;
+    private float yDir;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        animator = GetComponent<Animator>();
         
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+        yDir = Input.GetAxis("Vertical");
+        xDir = Input.GetAxis("Horizontal");
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            // 按下shift跑步
+            yDir += 1;
+        }
+
+        if (yDir>0)
+        {
+            animator.SetFloat("XSpeed", Mathf.Lerp(animator.GetFloat("XSpeed"), xDir, Time.deltaTime * animaSpeed));
+            animator.SetFloat("YSpeed", Mathf.Lerp(animator.GetFloat("YSpeed"), yDir, Time.deltaTime * animaSpeed));
+        }
+
+        transform.rotation *= Quaternion.Euler(transform.up * Input.GetAxis("Mouse X"));
+
     }
 }
