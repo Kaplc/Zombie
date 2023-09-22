db192f817f4dbbb9191d32fffcaba4482ef208c1
Kaplc <755536175@qq.com>
2023-09-22 22:18:18 +0800

    完成趴下动作

--- Assets/Script/GameScene/Object/Player.cs
+++ Assets/Script/GameScene/Object/Player.cs
@@ -16,56 +16,105 @@ public class Player : MonoBehaviour
     private bool isCrouch; // 蹲下
     private Coroutine crouchCoroutine;
 
+    private bool isProne;
+    private Coroutine proneCoroutine;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        
     }
 
     private void Update()
     {
+        Move();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");
+    }
+
+    private void Move()
+    {
         yDir = Input.GetAxis("Vertical");
         xDir = Input.GetAxis("Horizontal");
-
+        
+        // 按下shift跑动
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            // 按下shift跑动
-            if (yDir>0)
+            // 在后退时不能跑动
+            if (yDir > 0)
             {
-                // 在后退时不能跑动
                 yDir += 1;
             }
 
-            if (xDir>0)
+            if (xDir > 0)
             {
                 xDir += 1;
             }
 
-            if (xDir<0)
+            if (xDir < 0)
             {
                 xDir -= 1;
             }
         }
 
+        // 禁止s后退时左右移动
         if (yDir < 0)
         {
-            // 禁止s后退时左右移动
             xDir = 0;
         }
-        
+
+        // 按下c蹲下
         if (Input.GetKeyDown(KeyCode.C))
         {
-            // 按下c蹲下
             if (crouchCoroutine != null)
             {
                 StopCoroutine(crouchCoroutine);
             }
+
             isCrouch = !isCrouch;
             crouchCoroutine = StartCoroutine(CrouchAnimation(isCrouch));
         }
+
+        #region 趴下
+
+        // 按下z趴下
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            // 当前状态非蹲下不用重置
+            if (animator.GetLayerWeight(animator.GetLayerIndex("Crouch")) > 0.1f)
+            {
+                // 重置蹲下分层权重
+                if (crouchCoroutine != null)
+                {
+                    StopCoroutine(crouchCoroutine);
+                }
+
+                isCrouch = !isCrouch;
+                crouchCoroutine = StartCoroutine(CrouchAnimation(isCrouch));
+            }
+
+            // 触发prone参数
+            isProne = !isProne;
+            animator.SetBool("Prone", isProne);
+        }
+
+        // 趴下禁止左右
+        if (isProne)
+        {
+            xDir = 0;
+        }
+
+        #endregion
         
         // 人物移动
         animator.SetFloat("XSpeed", Mathf.Lerp(animator.GetFloat("XSpeed"), xDir, Time.deltaTime * moveSpeed));
@@ -74,6 +123,13 @@ public class Player : MonoBehaviour
         transform.rotation *= Quaternion.Euler(transform.up * Input.GetAxis("Mouse X"));
     }
 
+    #region 动作过度协程
+
+    /// <summary>
+    /// 蹲下优化动画协程
+    /// </summary>
+    /// <param name="isCrouch"></param>
+    /// <returns></returns>
     private IEnumerator CrouchAnimation(bool isCrouch)
     {
         if (isCrouch)
@@ -94,4 +150,14 @@ public class Player : MonoBehaviour
             }
         }
     }
+
+    #endregion
+
+    #region 动作事件
+
+    public void KnifeEvent()
+    {
+    }
+
+    #endregion
 }
