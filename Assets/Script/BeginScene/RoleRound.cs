e916852911bbcf4503516493b5b1c37c0268ba79
Kaplc <755536175@qq.com>
2023-10-02 17:04:53 +0800

    完成选择角色面板可以拖动模型

---
+++ Assets/Script/BeginScene/RoleRound.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleRound : MonoBehaviour
+{
+    public float speed;
+    private bool allowRota;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), 200, 1<< LayerMask.NameToLayer("Player")))
+            {
+                allowRota = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            allowRota = false;
+        }
+
+        if (allowRota)
+        {
+            transform.Rotate(transform.up, -Input.GetAxis("Mouse X") * Time.deltaTime * speed);
+        }
+    }
+}
